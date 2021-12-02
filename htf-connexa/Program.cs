d40fe236811b61b89d0f4f161ba7fd0de6a48e3d
s108909 <s108909@ap.be>
2021-12-02 09:49:47 +0100

    init

---
+++ htf-connexa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htf_connexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
